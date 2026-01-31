ebca817289dd31a83f5246ecd0b0f48503e7af57
udayvb0208 <166993530+udayvb0208@users.noreply.github.com>
2026-01-31 13:49:03 +0530

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,5 +1,11 @@
-const socket = io();
+const socket = io("real-time-collaborative-drawing-canvas-production-c72a.up.railway.app", {
+  transports: ["websocket"],
+});
+
+
+
 
 socket.on("connect", () => {
   console.log("Connected to server:", socket.id);
 });
+
