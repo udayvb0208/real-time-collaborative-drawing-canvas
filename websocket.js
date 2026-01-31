db2c6820c6c2437499fe378c5d4449e507c793a4
udayvb0208 <166993530+udayvb0208@users.noreply.github.com>
2026-01-31 14:09:03 +0530

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,12 +1,10 @@
 const socket = io("https://real-time-collaborative-drawing-canvas-production-c72a.up.railway.app", {
   transports: ["websocket"],
 });
-socket.io.opts.reconnection = true;
-socket.io.opts.timeout = 20000;
-
 
 socket.on("connect", () => {
   console.log("Connected to server:", socket.id);
 });
+
 
 
